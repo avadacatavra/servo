44190c9e5ad6ada6b90dc55e53f4693843f2a58f
ddh <dianehosfelt@gmail.com>
2016-10-17 10:46:35 +0100

    works in servo!!!!!!!! some asserts commented out

--- tests/wpt/web-platform-tests/resources/testharness_xow.js
+++ tests/wpt/web-platform-tests/resources/testharness_xow.js
@@ -19,8 +19,8 @@ policies and contribution forms [3].
     var settings = {
         output:true,
         harness_timeout:{
-            "normal":10000,
-            "long":60000
+            "normal":10000000,
+            "long":60000000
         },
         test_timeout:null,
         message_events: ["start", "test_state", "result", "completion"]
